525ba2c23804421ce4a88688f001a3b77975a2b0
Graham Arnett <GrahamKentArnett@outlook.com>
2025-01-11 13:35:56 -0500

    init commit

---
+++ PasswordGenApp/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace PasswordGenApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my Password Generator App!");
+            Random random = new Random();
+            string choice;
+            char[] letters = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+            char[] numbers = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+            char[] specialSyms = {'!','@','#','$','%','^','&','*','(',')','_','-','+','='};
+
+            do
+            {
+                Console.Write("\nPlease enter a number for how long you want your password: ");
+
+                int passSize = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                for (int i = passSize -1; i >= 0; i--)//originally it was i=passSize and the last i was an increment but that filled the page.
+                {
+                    int symbolType = random.Next(1,4);
+                    int letterChoice = random.Next(1,27);
+                    int numChoice = random.Next(1,11);
+                    int symChoice = random.Next(1,15);
+                    //might come back to this with a switch statement version
+                    if (symbolType == 1)
+                    {
+                        Console.Write(letters[letterChoice]);
+                    }
+                    else if (symbolType == 2)
+                    {
+                        Console.Write(numbers[numChoice]);
+                    }
+                    else
+                    {
+                        Console.Write(specialSyms[symChoice]);
+                    }
+                }
+
+            Console.Write("\nWould you like to generate a new password? (y/n): ");
+            choice = Console.ReadLine().ToLower();
+            } while (choice == "y");
+        }
+    }
+}

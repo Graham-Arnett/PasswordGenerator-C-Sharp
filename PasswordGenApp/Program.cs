06b3b449c023c0f3d683a9a9a1737efe2052ad2f
Graham Arnett <GrahamKentArnett@outlook.com>
2025-01-11 13:46:13 -0500

    fixed my issue, forgot arrays begin at index position zero

--- PasswordGenApp/Program.cs
+++ PasswordGenApp/Program.cs
@@ -8,9 +8,9 @@
             Random random = new Random();
             string choice;
             char[] letters = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-            char[] numbers = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+            char[] numbers = { '1', '2', '3', '4', '5', '6', '7', '8', '9','0'};
             char[] specialSyms = {'!','@','#','$','%','^','&','*','(',')','_','-','+','='};
-
+    
             do
             {
                 Console.Write("\nPlease enter a number for how long you want your password: ");
@@ -20,9 +20,9 @@
                 for (int i = passSize -1; i >= 0; i--)//originally it was i=passSize and the last i was an increment but that filled the page.
                 {
                     int symbolType = random.Next(1,4);
-                    int letterChoice = random.Next(1,27);
-                    int numChoice = random.Next(1,11);
-                    int symChoice = random.Next(1,15);
+                    int letterChoice = random.Next(0,26);
+                    int numChoice = random.Next(0,10);
+                    int symChoice = random.Next(0,14);
                     //might come back to this with a switch statement version
                     if (symbolType == 1)
                     {

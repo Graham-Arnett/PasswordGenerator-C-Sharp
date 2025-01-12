7be3dc34f895683450c126db2a4af5869815f865
Graham Arnett <GrahamKentArnett@outlook.com>
2025-01-12 15:43:50 -0500

    added validation

--- PasswordGenApp/Program.cs
+++ PasswordGenApp/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace PasswordGenApp
+﻿using System.Linq.Expressions;
+
+namespace PasswordGenApp
 {
     internal class Program
     {
@@ -10,43 +12,57 @@
             char[] letters = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
             char[] numbers = { '1', '2', '3', '4', '5', '6', '7', '8', '9','0'};
             char[] specialSyms = {'!','@','#','$','%','^','&','*','(',')','_','-','+','='};
-    
+            bool validInput = false;
             do
             {
-                Console.Write("\nPlease enter a number for how long you want your password: ");
 
-                int passSize = int.Parse(Console.ReadLine());
-                Console.WriteLine();
-                for (int i = passSize -1; i >= 0; i--)//originally it was i=passSize and the last i was an increment but that filled the page.
+                while (!validInput)
                 {
-                    int symbolType = random.Next(0,3);
-                    int letterChoice = random.Next(0,26);
-                    int numChoice = random.Next(0,10);
-                    int symChoice = random.Next(0,14);
-                    //might come back to this with a switch statement version
-                    //if (symbolType == 1)
-                    //{
-                    //    Console.Write(letters[letterChoice]);
-                    //}
-                    //else if (symbolType == 2)
-                    //{
-                    //    Console.Write(numbers[numChoice]);
-                    //}
-                    //else
-                    //{
-                    //    Console.Write(specialSyms[symChoice]);
-                    //}
-                    switch (symbolType) 
+                    Console.Write("\nPlease enter a number for how long you want your password: ");
+
+                    //int passSize = int.TryParse(Console.ReadLine());
+                    string passlength = Console.ReadLine();
+                    if (int.TryParse(passlength, out int passSize))
                     {
-                        case 0:
-                            Console.Write(letters[letterChoice]);
-                            break;
-                        case 1:
-                            Console.Write(numbers[numChoice]);
-                            break;
-                        case 2:
-                            Console.Write(specialSyms[symChoice]);
-                            break;
+
+                        for (int i = passSize - 1; i >= 0; i--)//originally it was i=passSize and the last i was an increment but that filled the page.
+                        {
+                            int symbolType = random.Next(0, 3);
+                            int letterChoice = random.Next(0, 26);
+                            int numChoice = random.Next(0, 10);
+                            int symChoice = random.Next(0, 14);
+                            //might come back to this with a switch statement version
+                            //if (symbolType == 1)
+                            //{
+                            //    Console.Write(letters[letterChoice]);
+                            //}
+                            //else if (symbolType == 2)
+                            //{
+                            //    Console.Write(numbers[numChoice]);
+                            //}
+                            //else
+                            //{
+                            //    Console.Write(specialSyms[symChoice]);
+                            //}
+                            switch (symbolType)
+                            {
+                                case 0:
+                                    Console.Write(letters[letterChoice]);
+                                    break;
+                                case 1:
+                                    Console.Write(numbers[numChoice]);
+                                    break;
+                                case 2:
+                                    Console.Write(specialSyms[symChoice]);
+                                    break;
+                            }
+                            validInput = true;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("That was invalid. Please enter an integer.");
+
                     }
                 }
 
